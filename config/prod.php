445a14c8ff5f405d579ed71033cef701760c0897
Jon Latorre <jonlatorremartinez@gmail.com>
2014-04-28 16:40:35 +0200

    preparando para usar doctrine

--- config/prod.php
+++ config/prod.php
@@ -4,3 +4,14 @@
 
 $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_mysql',
+	 'host'      => 'mysql_read.someplace.tld',
+         'dbname'    => 'my_database',
+         'user'      => 'my_username',
+         'password'  => 'my_password',
+         'charset'   => 'utf8',
+    ),
+));
