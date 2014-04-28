a4f076293a27cbae5126a61fa62e0b47bbe90e9f
root <info@etxea.net>
2014-04-28 23:21:03 +0200

    edicion de usuarios

--- src/app.php
+++ src/app.php
@@ -4,12 +4,17 @@ use Silex\Application;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
+
 use Silex\Provider\ServiceControllerServiceProvider;
 use Doctrine\DBAL\Connection;
+use Silex\Provider\FormServiceProvider;
 
 $app = new Application();
 $app->register(new UrlGeneratorServiceProvider());
 $app->register(new ValidatorServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'translator.messages' => array(),
+));
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new TwigServiceProvider());
 $app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
@@ -17,5 +22,7 @@ $app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
 
     return $twig;
 }));
+
+$app->register(new FormServiceProvider());
 
 return $app;
--- src/usuarios.php
+++ src/usuarios.php
@@ -21,12 +21,42 @@ $usuarios->match('/alta', function () use ($app) {
 ;
 
 $usuarios->match('/editar/{id}', function ($id) use ($app) {
+    $usuario = $app['db']->fetchAssoc('SELECT * FROM usuarios WHERE id = ?',array($id));
+    $form = $app['form.factory']->createBuilder('form', $usuario)
+        ->add('nombre')
+        ->add('apellidos')
+        ->add('username')
+        ->add('password')
+        ->add('categoria', 'choice', array(
+            'choices' => array(1 => 'Jefe de servicio/sección', 2 => 'Adjunto', 3 => 'Residente'),
+            'expanded' => true,
+        ))
+        ->add('rol', 'choice', array(
+            'choices' => array(0 => 'estandar', 1 => 'administrador'),
+            'expanded' => true,
+        ))
+        ->getForm();
     if ($app['request']->getMethod() == "POST" ) {
-        $mensaje = "Vamos guardar";
+        $form->bind($app['request']);
+        if ($form->isValid()) {
+            $mensaje = "Vamos guardar";
+            $data = $form->getData();
+            var_dump($data);
+            //ahora a actualizar la BBDD
+            unset($data['id']);
+            $app['db']->update('usuarios',$data,array('id' => $id));
+            // hay que cambiar la pass en caldav
+        } else {
+            $mensaje = "Formulario mal";
+        }
+        
     } else {
         $mensaje = "vamos a editar";
+        
+            
     }
-    return $app['twig']->render('usuarios-editar.html', array('id_usuario'=> $id ,'mensaje'=>$mensaje));
+    
+    return $app['twig']->render('usuarios-editar.html', array('id_usuario'=> $id ,'mensaje'=>$mensaje,'usuario'=>$usuario,'form' => $form->createView()));
 })
 ->bind('usuarios-editar')
 ->assert('id', '\d+') //nos aseguramos que nos pasan un decimal
