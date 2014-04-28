65f2907ce17a9efd1a4c0d4dc4a6242900726e96
root <root@sabredav.(none)>
2014-04-28 15:46:25 +0200

    Preparando el controlador de usuarios

--- src/usuarios.php
+++ src/usuarios.php
@@ -7,16 +7,28 @@ $usuarios->get('/', function () use ($app) {
 ->bind('usuarios')
 ;
 
-$usuarios->get('/alta', function () use ($app) {
-    return $app['twig']->render('usuarios-alta.html', array());
+$usuarios->match('/alta', function () use ($app) {
+    if ($app['request']->getMethod() == "POST" ) {
+        echo "POST";
+        $mensaje = "Vamos a dar de alta";
+    } else {
+        $mensaje = "Introduce los datos";
+    }
+    return $app['twig']->render('usuarios-alta.html', array('mensaje' => $mensaje ));
 })
 ->bind('usuarios-alta')
 ;
 
-$usuarios->get('/editar/{id}', function ($id) use ($app) {
-    return $app['twig']->render('usuarios-alta.html', array());
+$usuarios->match('/editar/{id}', function ($id) use ($app) {
+    if ($app['request']->getMethod() == "POST" ) {
+        $mensaje = "Vamos guardar";
+    } else {
+        $mensaje = "vamos a editar";
+    }
+    return $app['twig']->render('usuarios-editar.html', array('id_usuario'=> $id ,'mensaje'=>$mensaje));
 })
 ->bind('usuarios-editar')
+->assert('id', '\d+') //nos aseguramos que nos pasan un decimal
 ;
 
 return $usuarios
